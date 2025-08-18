fda9a4379b00f519bd9a39e12d930cad4da9726e
Joe Webster <joseph.scott.webster@gmail.com>
2025-08-18 14:57:32 -0400

    Create useTimer hook

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,21 +1,51 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function useTimer() {
+  const timerIntervalId = useRef<null | NodeJS.Timeout>(null);
   const [isRunning, setIsRunning] = useState(false);
   const [timerSeconds, setTimerSeconds] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (isRunning) {
+      const intervalId = setInterval(() => {
+        setTimerSeconds((prevSeconds) => {
+          if (prevSeconds === null || prevSeconds <= 0) {
+            setIsRunning(false);
+            return 0;
+          }
+          return prevSeconds - 1;
+        });
+      }, 1000);
+
+      timerIntervalId.current = intervalId;
+    }
+  }, [isRunning]);
+
+  const startTimer = (seconds: number) => {
+    setTimerSeconds(seconds);
+    setIsRunning(true);
+  };
+
+  const cancelTimer = () => {
+    console.log("Cancel timer");
+    if (timerIntervalId.current) {
+      clearInterval(timerIntervalId.current);
+      timerIntervalId.current = null;
+      resetTimer();
+    }
+  };
 
   const resetTimer = () => {
     setTimerSeconds(null);
     setIsRunning(false);
   };
 
-  const startTimer = (seconds: number) => setTimerSeconds(seconds);
-
   return {
     timerSeconds,
     isRunning,
     setTimerSeconds,
     startTimer,
+    cancelTimer,
     resetTimer,
   };
 }
