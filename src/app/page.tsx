02cebf69363e5b634d68304739dbaf274c7670e9
Joe Webster <joseph.scott.webster@gmail.com>
2025-08-20 13:17:08 -0400

    Update styling and format remaining seconds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSpeechRecognition } from "@/hooks/useSpeechRecognition";
 import { useTimer } from "@/hooks/useTimer";
-import { findCommandType, parseDuration } from "@/utils/utils";
+import { findCommandType, parseDuration, formatTime } from "@/utils/utils";
 
 export default function Home() {
   const {
@@ -53,38 +53,43 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <button
-        onClick={toggleSpeechRecognition}
-        className={`w-fit p-3 rounded-md font-medium transition-colors ${
-          isListening
-            ? "bg-red-500 hover:bg-red-600 text-white"
-            : "bg-blue-500 hover:bg-blue-600 text-white"
-        }`}
-      >
-        {isListening ? "Stop Listening" : "Start Voice Recognition"}
-      </button>
+    <div className="h-dvh flex justify-center items-center p-4">
+      <div className="w-full flex flex-col gap-6 items-center">
+        <div className="border rounded-lg flex flex-col justify-center items-center w-full h-[400px] gap-6 max-w-5xl">
+          <div className="relative">
+            <p className="text-[80px] leading-none">
+              {formatTime(remainingSeconds ?? 0)}
+            </p>
 
-      <div>
-        <p>{remainingSeconds}</p>
-      </div>
-
-      {(isRunning || isPaused) && (
-        <div>
-          <button
-            onClick={stopTimer}
-            className="w-fit border p-3 rounded-md font-medium transition-colors"
-          >
-            Cancel
-          </button>
-          <button
-            onClick={isPaused ? resumeTimer : pauseTimer}
-            className="w-fit border p-3 rounded-md font-medium transition-colors"
-          >
-            {isPaused ? "Resume" : "Pause"}
-          </button>
+            {(isRunning || isPaused) && (
+              <div className="flex gap-4 absolute left-1/2 top-full transform -translate-x-1/2 mt-10">
+                <button
+                  onClick={stopTimer}
+                  className="border p-3 rounded-md font-medium transition-colors w-[86px]"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={isPaused ? resumeTimer : pauseTimer}
+                  className="border p-3 rounded-md font-medium transition-colors w-[86px]"
+                >
+                  {isPaused ? "Resume" : "Pause"}
+                </button>
+              </div>
+            )}
+          </div>
         </div>
-      )}
+        <button
+          onClick={toggleSpeechRecognition}
+          className={`w-fit p-3 rounded-md font-medium transition-colors ${
+            isListening
+              ? "bg-red-500 hover:bg-red-600 text-white"
+              : "bg-blue-500 hover:bg-blue-600 text-white"
+          }`}
+        >
+          {isListening ? "Stop Listening" : "Start Voice Recognition"}
+        </button>
+      </div>
     </div>
   );
 }
